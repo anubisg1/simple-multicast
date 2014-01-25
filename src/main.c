c2d9274e1e6fb2269ebb900210d43c6e9a899bc7
Andrea Florio <andrea@opensuse.org>
2014-01-26 00:13:10 +0100

    working on non-interactive functions. ipv4 and ipv6 clients can now run without the need of using the menu!

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@
 #include "ipv6/mcast6-server.h"
 #include "ipv6/mcast6-client.h"
 
-#define VERSION "0.2.3"
+#define VERSION "0.2.3.1"
 
 void clean_screen() {
 /* FIX ME - I don't like this solution */
@@ -36,7 +36,7 @@ void clean_screen() {
 #endif
 }
 
-void PressEnterToContinue() {
+void press_enter_to_continue() {
   int c;
   fflush(stdin);
   printf("Press ENTER to continue... ");
@@ -46,11 +46,11 @@ void PressEnterToContinue() {
 
 void print_help() {
     printf("Usage: ./multicast [PROTOCOL] [OPERATIONAL MODE] [OPTION] [OPTION] ... \n\n");
+	printf("  --version\tprint current version and exit\n");
+	printf("  --help\tprint this help and exit\n\n");
     printf("Run without arguments to run the interactive console.\n");
     printf("To run without interaction, use the options below.\n");
-	printf("  --version\tprint current version and exit");
-	printf("  --help\tprint this help and exit");
-    printf("One per each group is required\n\n");
+    printf("One per each group is required.\n\n");
     printf("IP protocol version:\n");
     printf("  -v4\t\t\twill use IPv4\n");
     printf("  -v6\t\t\twill use IPv6\n\n");
@@ -63,18 +63,25 @@ void print_help() {
     printf("  -source [IP ADDRESS]\tONLY if '-client-ssm' is used\n\t\t\tSpecify the IP address of the authorized source\n");
     printf("  -port [NUMBER]\tSelect the port number. Below 1024 requires\n\t\t\tadministator rights\n");
     printf("  -delay [NUMBER]\tONLY if '-server' is used\n\t\t\tSet the delay, in seconds, beteween packets\n");
+    printf("Example of usage:\n");
+    printf("  multicast -v4 -server -group 239.1.2.3 -port 9999 -delay 1\n");
+    printf("  multicast -v6 -client -group FF05::3232 -port 9999\n");
+    printf("  multicast -v4 -client-ssm -group 232.1.2.3 -source 10.0.0.1 -port 9999\n");
 }
 
-int main(int argc, char *argv[]) {
+void print_error_and_exit() {
+    printf("invalid options. Run again with \"--help\" for usage manual\n");
+}
+
+void interactive_menu() {
+
    char mcast_group[INET6_ADDRSTRLEN] = "0.0.0.0";
    char ssm_source[INET6_ADDRSTRLEN] = "0.0.0.0";
-   struct in6_addr ip_address;
    int destination_port = 1;
    int delay = 1;
+   struct in6_addr ip_address;
    char choice[100] = "0";
 
-   clean_screen();
-   printf("simple-multicast version %s\n\n",  VERSION);
    do {
     printf("Menu:\n\n");
     printf("1. IPv4 - Multicast Server\n");
@@ -97,7 +104,6 @@ int main(int argc, char *argv[]) {
             if (destination_port >= 0 && destination_port <= 65535) {
                     printf("Enter the interval between packets in seconds: ");
                     scanf("%d", &delay);
-
                     /* we are ready , let start the server */
                     mcast_server(mcast_group, destination_port, delay);
             }
@@ -108,7 +114,7 @@ int main(int argc, char *argv[]) {
         else {
             printf("Invalid multicast group address!\n");
         }
-        PressEnterToContinue();
+        press_enter_to_continue();
         clean_screen();
     }
     else if (strcmp(choice,"2") == 0 ) {
@@ -116,7 +122,7 @@ int main(int argc, char *argv[]) {
         scanf("%s", mcast_group);
 
         if (is_valid_mcast_ip4(mcast_group) == TRUE ) {
-			printf("Enter the port number to listen to: ");
+            printf("Enter the port number to listen to: ");
             scanf("%d", &destination_port);
             if (destination_port >= 0 && destination_port <= 65535) {
 
@@ -130,7 +136,7 @@ int main(int argc, char *argv[]) {
         else {
             printf("Invalid multicast group address!\n");
         }
-        PressEnterToContinue();
+        press_enter_to_continue();
         clean_screen();
     }
     else if (strcmp(choice,"3") == 0 ) {
@@ -161,7 +167,7 @@ int main(int argc, char *argv[]) {
         else {
             printf("Invalid multicast group address!\n");
         }
-        PressEnterToContinue();
+        press_enter_to_continue();
         clean_screen();
     }
     else if (strcmp(choice,"4") == 0 ) {
@@ -187,7 +193,7 @@ int main(int argc, char *argv[]) {
         else {
             printf("Invalid multicast group address!\n");
         }
-        PressEnterToContinue();
+        press_enter_to_continue();
         clean_screen();
     }
     else if (strcmp(choice,"5") == 0 ) {
@@ -210,7 +216,7 @@ int main(int argc, char *argv[]) {
         else {
             printf("Invalid multicast group address!\n");
         }
-        PressEnterToContinue();
+        press_enter_to_continue();
         clean_screen();
     }
     else if (strcmp(choice,"6") == 0 ) {
@@ -245,7 +251,7 @@ int main(int argc, char *argv[]) {
         else {
             printf("Invalid multicast group address!\n");
         }
-        PressEnterToContinue();
+        press_enter_to_continue();
         clean_screen();
 
     }
@@ -254,10 +260,80 @@ int main(int argc, char *argv[]) {
     }
     else {
         printf("Invalid choice! \n");
-        PressEnterToContinue();
+        press_enter_to_continue();
         clean_screen();
        }
    } while (strcmp(choice,"7") != 0);
+}
+
+int main(int argc, char *argv[]) {
+
+   int destination_port = 1;
+   struct in6_addr ip_address;
+
+   if (argc == 1) {
+        interactive_menu();
+   }
+   else if (argc == 2) {
+        if (strcmp(argv[1],"--help") == 0) {
+            print_help();
+        }
+        else if (strcmp(argv[1],"--version") == 0) {
+            printf("version %s\n",  VERSION);
+        }
+        else {
+            printf("invalid option. Run again with \"--help\" for usage manual\n");
+        }
+   }
+   else if (argc == 7) {
+        if ((strcmp(argv[2],"-client") == 0) && (strcmp(argv[3],"-group") == 0) && (strcmp(argv[5],"-port") == 0)) {
+            if (strcmp(argv[1],"-v4") == 0) { // we are using IPv4
+                if (is_valid_mcast_ip4(argv[4]) == TRUE ) {
+                        destination_port = atoi(argv[6]);
+                        if (destination_port >= 0 && destination_port <= 65535) {
+                            /* we are ready , let start the client */
+                            mcast_client(argv[4], destination_port);
+                        }
+                        else {
+                            printf("Invalid port number!\n");
+                        }
+                }
+                else {
+                    printf("Invalid multicast group address!\n");
+                }
+            }
+            else if (strcmp(argv[1],"-v6") == 0) { // we are using IPv4
+                inet_pton(AF_INET6, argv[4], &ip_address);
+                if (IN6_IS_ADDR_MULTICAST(&ip_address) == TRUE ) {
+                        destination_port = atoi(argv[6]);
+                        if (destination_port >= 0 && destination_port <= 65535) {
+                            /* we are ready , let start the client */
+                            mcast6_client(argv[4], destination_port);
+                        }
+                        else {
+                            printf("Invalid port number!\n");
+                        }
+                }
+                else {
+                    printf("Invalid multicast group address!\n");
+                }
+
+            }
+            else {
+                print_error_and_exit();
+            }
+        }
+        else {
+            print_error_and_exit();
+        }
+
+   }
+   else if (argc == 9) {
+        printf("work in progress :-)\n");
+   }
+   else {
+        print_error_and_exit();
+   }
 
    return 0;
 }
